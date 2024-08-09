fb63265716f17ed81b020b9ccb8345efa424f3e2
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-08-09 11:28:58 +0500

    MementoMain

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,7 @@
-import memento.Document;
-import memento.DocumentHistory;
+import memento.MementoMain;
 
 public class Main {
     public static void main(String[] args) {
-        Document doc = new Document();
-        DocumentHistory history = new DocumentHistory();
-
-        doc.setContent("this is a new content");
-        history.push(doc.makeSnapshot());
-        doc.setFontSize(22);
-        history.push(doc.makeSnapshot());
-        doc.setFontSize(32);
-        history.push(doc.makeSnapshot());
-        doc.setFontName("q");
-        history.push(doc.makeSnapshot());
-
-        doc.setFontName("qwe");
-        history.push(doc.makeSnapshot());
-
-        doc.setFontSize(69);
-        history.push(doc.makeSnapshot());
-
-        history.pop();
-        doc.restore(history.pop());
-        System.out.println(doc);
+        MementoMain.execute();
     }
 }
---
+++ src/memento/MementoMain.java
@@ -0,0 +1,28 @@
+package memento;
+
+
+public class MementoMain {
+    public static void execute() {
+        Document doc = new Document();
+        DocumentHistory history = new DocumentHistory();
+
+        doc.setContent("this is a new content");
+        history.push(doc.makeSnapshot());
+        doc.setFontSize(22);
+        history.push(doc.makeSnapshot());
+        doc.setFontSize(32);
+        history.push(doc.makeSnapshot());
+        doc.setFontName("q");
+        history.push(doc.makeSnapshot());
+
+        doc.setFontName("qwe");
+        history.push(doc.makeSnapshot());
+
+        doc.setFontSize(69);
+        history.push(doc.makeSnapshot());
+
+        history.pop();
+        doc.restore(history.pop());
+        System.out.println(doc);
+    }
+}
