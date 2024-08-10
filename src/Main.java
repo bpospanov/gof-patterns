43d06b28caa44ffd999d51fce7030c5a3f4391f4
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-08-10 12:50:41 +0500

    State pattern example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-import memento.MementoMain;
+import state.BrushTool;
+import state.Canvas;
+import state.SelectionTool;
+import state.EraserTool;
 
 public class Main {
     public static void main(String[] args) {
-        MementoMain.execute();
+        var canvas = new Canvas();
+        canvas.setCurrentTool(new SelectionTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new BrushTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new EraserTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
     }
+
 }
